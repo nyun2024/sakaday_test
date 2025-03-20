844ff89b8eac978f00bd76d7c60c3b50952e1d28
nyun2024 <kws043945@gmail.com>
2025-03-20 16:47:32 +0900

    폰트 추가, module.scss 적용 테스트

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import dummy from '@img/dummy.jpg'
+import styles from './Home.module.scss'
+import Container from '@components/Container'
 
 const Home = () => {
   return (
-    <>
-      <div>Home</div>
+    <Container>
+      <h1 className={styles.title}>사카모토 데이즈 능력 고사</h1>
       <img src={dummy} alt="dummy" />
-    </>
+    </Container>
   )
 }
 
