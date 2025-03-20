3871d78977adbd06aaaa1dd08210db197bc1ae09
nyun2024 <kws043945@gmail.com>
2025-03-20 15:20:13 +0900

    오류 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,6 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-
-createRoot(document.getElementById('root')).render()
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './router/index.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
-    <RouterProvider router={router} />
-  </>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />)
