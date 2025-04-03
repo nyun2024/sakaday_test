6a9dc6044a874fa09b6972ad0a8ad0966b092c23
nyun2024 <kws043945@gmail.com>
2025-04-04 00:06:31 +0900

    점수 결과 페이지 스타일 적용 중

--- src/pages/result/ResultScore.jsx
+++ src/pages/result/ResultScore.jsx
@@ -4,6 +4,7 @@ import img0to49 from '@img/score/0to49.jpg'
 import img50to69 from '@img/score/50to69.jpg'
 import img70to89 from '@img/score/70to89.jpg'
 import img90to100 from '@img/score/90to100.jpg'
+import styles from './ResultScore.module.scss'
 
 const ResultScore = () => {
   const userScore = localStorage.getItem('userScore')
@@ -24,19 +25,19 @@ const ResultScore = () => {
   const scoreView = {
     score0to49: {
       img: img0to49,
-      dialogue: '하아 실망이야~ 다시 1권부터 정독해봐~!',
+      dialogue: '"하아 실망이야~\n다시 1권부터 정독해봐~!"',
     },
     score50to69: {
       img: img50to69,
-      dialogue: '흐음 이 점수.. 좀 더 분발해야 겠는걸?',
+      dialogue: '"흐음 이 점수..\n좀 더 분발해야 겠는걸?"',
     },
     score70to89: {
       img: img70to89,
-      dialogue: '꽤 봐 줄만한 점수인걸? 열심히 공부했나봐?!',
+      dialogue: '"꽤 봐 줄만한 점수인걸?\n열심히 공부했나봐?!"',
     },
     score90to100: {
       img: img90to100,
-      dialogue: '사카데이 잘.알. \n최고예요👍',
+      dialogue: '"사카데이 잘.알. \n최고예요👍"',
     },
   }
 
@@ -47,11 +48,23 @@ const ResultScore = () => {
   else resultData = scoreView.score90to100
 
   return (
-    <Container>
-      <div>{userName}님의 점수는</div>
-      <div>{userScore}점</div>
-      <img src={resultData.img} alt="score image" />
-      <p>{resultData.dialogue}</p>
+    <Container className={styles.resultScoreContainer} center>
+      <div className={styles.userResultScore}>
+        <span className={styles.username}>{userName}</span>님의 점수는
+        <br />
+        <span className={styles.score}>
+          <span className={styles.scoreNum}>{userScore}</span>점
+        </span>
+      </div>
+      <img src={resultData.img} className={styles.scoreImg} alt="score image" />
+      <p className={styles.dialogue}>
+        {resultData.dialogue.split('\n').map((line, index) => (
+          <React.Fragment key={index}>
+            {line}
+            <br />
+          </React.Fragment>
+        ))}
+      </p>
     </Container>
   )
 }
