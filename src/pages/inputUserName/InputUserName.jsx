2893eff80685daea0ff7a7e2ed104f952fd8bd14
nyun2024 <kws043945@gmail.com>
2025-05-09 21:41:15 +0900

    이름 입력화면 desc 삭제제

--- src/pages/inputUserName/InputUserName.jsx
+++ src/pages/inputUserName/InputUserName.jsx
@@ -22,15 +22,6 @@ const InputUserName = () => {
   return (
     <Container center className={styles.inputUserNameContainer}>
       <div className={styles.title}>이름을 입력해 주세요.</div>
-      <div className={styles.desc}>
-        ※ 콜리 유저는{' '}
-        <strong>
-          '<span>콜리 닉네임</span>'
-        </strong>
-        으로
-        <br />
-        입력 부탁드립니다.
-      </div>
       <input type="text" className={styles.input} onChange={handleInputChange} />
       <GoLink className={styles.goTest} link="/Quiz" disabled={userName.length === 0} clickEvent={clickLinkEvent}>
         확인
